33ff3e761101313e5b694833f37d442cb9f627e8
namansinghal <72150803+namansinghal111@users.noreply.github.com>
2024-07-13 01:04:53 +0530

    Update server.js

--- server.js
+++ server.js
@@ -98,7 +98,7 @@ cron.schedule("0 0 1 * * *", async () => {
 app.use(express.json()); //pass incoming json data
 app.use(cookieParser()); //pass the cookie automatically
 const corsOptions = {
-  origin: "http://localhost:3000",
+  origin: "https://localhost:3000",
   credentials: true,
 };
 app.use(cors(corsOptions));
