8c86b7307e74de154144b905620efe58ae79409d
namansinghal <72150803+namansinghal111@users.noreply.github.com>
2024-07-13 10:22:08 +0530

    Update server.js

--- server.js
+++ server.js
@@ -102,8 +102,8 @@ const corsOptions = {
     //"Access-Control-Allow-Origin": "http://localhost:8000/",
     "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
       "withCredentials" : false,
-   "Access-Control-Allow-Headers": "Origin, Accept, X-Requested-With, Content-Type"
-   "Accept": 'application/json',
+   "Access-Control-Allow-Headers": "Origin, Accept, X-Requested-With, Content-Type",
+   Accept: 'application/json',
    'Content-Type': 'application/json'
    
   },
